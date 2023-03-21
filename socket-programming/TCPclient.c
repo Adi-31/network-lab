f242f7356c588cf2f66856b6e8fe55b25e32ccc5
Adi-31 <mail4adithya31@gmail.com>
2023-03-21 20:21:09 +0530

    added comments to TCP socket pgms

--- socket-programming/TCPclient.c
+++ socket-programming/TCPclient.c
@@ -8,22 +8,22 @@
 
 int main()
 {
-    int client_socket;
-    struct sockaddr_in server_address;
+    int client_socket;                 // client_socket is the file descriptor
+    struct sockaddr_in server_address; // creating structure of the type sockaddr_in
     char buffer[1024];
 
     // create a socket
     client_socket = socket(AF_INET, SOCK_STREAM, 0);
 
     // set server address properties
-    server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
-    server_address.sin_port = htons(12345);
+    server_address.sin_family = AF_INET;         // setting the family of the server address to AF_INET
+    server_address.sin_addr.s_addr = INADDR_ANY; // setting the IP address of the server address to INADDR_ANY
+    server_address.sin_port = htons(12345);      // setting the port number of the server address to 12345
 
     // connect to the server
     connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address));
 
-    // get input message from user
+    // get input message from user and store it in the buffer
     printf("Enter message: ");
     fgets(buffer, sizeof(buffer), stdin);
 
--- socket-programming/TCPserver.c
+++ socket-programming/TCPserver.c
@@ -8,34 +8,34 @@
 
 int main()
 {
-    int server_socket, client_socket;
-    struct sockaddr_in server_address, client_address;
+    int server_socket, client_socket;                  // client_socket and server_socket are file descriptors
+    struct sockaddr_in server_address, client_address; // creating structures of the type sockaddr_in
     char buffer[1024];
 
     // create a socket
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
     // set server address properties
-    server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = INADDR_ANY;
-    server_address.sin_port = htons(12345);
+    server_address.sin_family = AF_INET;         // setting the family of the server address to AF_INET
+    server_address.sin_addr.s_addr = INADDR_ANY; // setting the IP address of the server address to INADDR_ANY
+    server_address.sin_port = htons(12345);      // setting the port number of the server address
 
     // bind the socket to the address
     bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address));
 
     // start listening for incoming connections
-    listen(server_socket, 1);
+    listen(server_socket, 5); // 5 is the number of clients that can connect to the server
     printf("Server is listening for incoming connections...\n");
 
     // accept a new connection
-    socklen_t client_address_size = sizeof(client_address);
-    client_socket = accept(server_socket, (struct sockaddr *)&client_address, &client_address_size);
+    socklen_t client_address_size = sizeof(client_address);                                          // storing the length of the client address
+    client_socket = accept(server_socket, (struct sockaddr *)&client_address, &client_address_size); // accepting the client connection
     printf("Connected by %s:%d\n", inet_ntoa(client_address.sin_addr), ntohs(client_address.sin_port));
 
     // receive data from client
-    memset(buffer, 0, sizeof(buffer));
-    read(client_socket, buffer, sizeof(buffer));
-    printf("Received message: %s\n", buffer);
+    memset(buffer, 0, sizeof(buffer));           // memset is used to fill a block of memory with a particular value (in this case, with 0)
+    read(client_socket, buffer, sizeof(buffer)); // reading the message from the client and storing it in buffer
+    printf("Received message: %s\n", buffer);    // display the message in the buffer
 
     // close the connection
     close(client_socket);
